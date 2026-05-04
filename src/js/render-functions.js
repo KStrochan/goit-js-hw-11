b32a253cabbc1faf85e309cda5b8a132f3e92104
Konstantyn <72542265+KStrochan@users.noreply.github.com>
2026-05-04 21:30:41 +0300

    Update render-functions.js

--- src/js/render-functions.js
+++ src/js/render-functions.js
@@ -1,27 +1,50 @@
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
+let lightbox;  // Зберігаємо екземпляр lightbox
+
 export const createGallery = (images) => {
   const gallery = document.querySelector('.gallery');
   const markup = images.map(image => {
     return `
-      <a href="${image.largeImageURL}">
-        <img src="${image.webformatURL}" alt="${image.tags}" />
-      </a>
+      <li>
+        <a href="${image.largeImageURL}">
+          <img src="${image.webformatURL}" alt="${image.tags}" />
+        </a>
+        <div class="info">
+          <p>Likes: ${image.likes}</p>
+          <p>Views: ${image.views}</p>
+          <p>Comments: ${image.comments}</p>
+          <p>Downloads: ${image.downloads}</p>
+        </div>
+      </li>
     `;
   }).join('');
 
   gallery.innerHTML = markup;
+
+  // Ініціалізація SimpleLightbox після рендеру галереї
+  if (!lightbox) {
+    lightbox = new SimpleLightbox('.gallery a', { 
+      captionsData: 'alt', 
+      captionDelay: 250 
+    });
+  } else {
+    lightbox.refresh();  // Оновлюємо галерею
+  }
 };
 
 export const clearGallery = () => {
   const gallery = document.querySelector('.gallery');
-  gallery.innerHTML = '';  // очищаємо галерею
+  gallery.innerHTML = '';  // очищуємо галерею
 };
 
 export const showLoader = () => {
   const loader = document.querySelector('.loader');
-  loader.classList.add('is-visible');  // Показуємо лоадер
+  loader.classList.add('is-visible');
 };
 
 export const hideLoader = () => {
   const loader = document.querySelector('.loader');
-  loader.classList.remove('is-visible');  // Ховаємо лоадер
+  loader.classList.remove('is-visible');
 };
